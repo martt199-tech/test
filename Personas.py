d6f4d42ee39db92504e6815df403255b07897a95
msastre-stratio <msastre@stratio.com>
2020-10-01 13:59:19 +0200

    Style changes

--- Personas.py
+++ Personas.py
@@ -20,16 +20,16 @@ class Trabajador(Persona):
     
     #Constructor de la clase supervisor
     def __init__(self,nombre,edad,departamento,puesto):
+        #Invocacion al constructor de la clase Persona
         Persona.__init__(self, nombre, edad)
         self.departamento = "data"
         self.puesto = "analyst"
-        #Invocacion al constructor de la clase Persona
            
     def presentation1(self):
             Persona.presentation(self),
             print(f"hola!, mi departamneto es {self.departamento} y mi puesto {self.puesto}")
           
         
-       # manuel = Trabajador('Alberto','23','Data','Developer')
-       # manuel.presentation
+       t1 = Trabajador('Alberto','23','Data','Developer')
+       t1.presentation()
             
