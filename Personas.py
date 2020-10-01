dc10fbd6fe53159c75cd465ea77ed94fb5d5d8c4
msastre-stratio <msastre@stratio.com>
2020-10-01 13:56:15 +0200

    Python Exercises

--- Personas.py
+++ Personas.py
@@ -6,7 +6,7 @@ Created on Wed Sep 30 09:08:50 2020
 @author: msastre
 """
 
-class Persona:
+class Persona(object):
     def __init__ (self,nombre,edad):
         self.nombre = nombre
         self.edad=edad
@@ -15,3 +15,21 @@ class Persona:
         print(f"Hola! Soy {self.nombre} y tengo {self.edad} años")
         
         
+
+class Trabajador(Persona):
+    
+    #Constructor de la clase supervisor
+    def __init__(self,nombre,edad,departamento,puesto):
+        Persona.__init__(self, nombre, edad)
+        self.departamento = "data"
+        self.puesto = "analyst"
+        #Invocacion al constructor de la clase Persona
+           
+    def presentation1(self):
+            Persona.presentation(self),
+            print(f"hola!, mi departamneto es {self.departamento} y mi puesto {self.puesto}")
+          
+        
+       # manuel = Trabajador('Alberto','23','Data','Developer')
+       # manuel.presentation
+            
